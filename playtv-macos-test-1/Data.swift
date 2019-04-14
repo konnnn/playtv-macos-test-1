f5e136c4e642cfc159cb874aaa58d0cb8ac0d77f
Евгений Конкин <evgeniykonkin@gmail.com>
2019-04-14 22:51:46 +0400

    Install RealmSwift Pods for macOS

---
+++ playtv-macos-test-1/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  playtv-macos-test-1
+//
+//  Created by Evgeny Konkin on 14.04.2019.
+//  Copyright © 2019 Evgeny Konkin. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Data: NSObject {
+    
+}
